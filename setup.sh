e67e092c2e6d15465e60ee1c7477ba9516153ab6
Avansim-Chris <chris.hammond@avansimgroup.com>
2026-06-10 12:15:21 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ cd
 echo "--> Installing permenant version of OnLogic PSE (IO/CAN/DIO/Serial) Driver..."
 sudo apt-get install build-essential flex bison libssl-dev libelf-dev
 sudo apt-get install linux-headers-$(uname -r)
-cd pse_heci
+cd ~/pse_heci
 cd src
 sudo make all
 sudo cp pse.ko /lib/modules/
