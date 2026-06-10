e07f59cedb6672b45a774ad5e25d0a804bdc01a6
Avansim-Chris <chris.hammond@avansimgroup.com>
2026-06-10 12:51:57 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -79,8 +79,6 @@ sudo apt-get install linux-headers-$(uname -r)
 cd pse_heci
 cd src
 sudo make all
-sudo insmod pse.ko && dmesg
-cd
 sudo cp pse.ko /lib/modules/
 $(uname -r)/kernel/drivers/hid/intel-ish-hid/
 echo 'pse' | sudo tee -a /etc/modules-load.d/modules.conf$ sudo depmod -a
